adfc6a6a611e30a1851ce42c06e220a3e90b0087
chengpeng <chengpeng@yitong.com.cn>
2023-02-28 14:44:39 +0800

    stack

---
+++ data-structures/stack.js
@@ -0,0 +1,44 @@
+class Stack {
+    constructor() {
+        this.count = 0;
+        this.items = []
+    }
+    push(item) {
+        this.items.push(item);
+        this.count++
+    }
+    pop() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        this.items.pop()
+        this.count--
+    }
+
+    peek() {
+        return this.items[this.count - 1];
+    }
+
+    isEmpty() {
+        return this.count === 0
+    }
+
+    size() {
+        return this.count
+    }
+
+    clear() {
+        this.items = []
+        this.count = 0
+    }
+
+}
+
+let stack = new Stack()
+stack.push(1)
+stack.push(2)
+stack.pop()
+console.log("isEmpty", stack.isEmpty())
+console.log("size", stack.size())
+console.log("peek", stack.peek())
+console.log(stack)
